14bf978da695990836ecc1fe7e8f6ca3de037f2f
Alkihis <alkihis@gmail.com>
2020-09-25 14:30:20 +0200

    Better exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@
 export { default as default } from './client';
 export * from './client';
 export * from './types';
+export * from './v1/types.v1';
+export * from './v2/types.v2';
+export * from './v2/TweetPaginator';
