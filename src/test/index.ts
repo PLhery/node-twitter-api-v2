75a481d8d0d768fcd171512c6afbad38c3b12183
Louis <alkihis@gmail.com>
2021-04-17 17:13:44 +0200

    Removed commander dependency

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,22 +1,19 @@
 import { TwitterApi } from '..';
-import commander from 'commander';
 import dotenv from 'dotenv';
 import { getAccessClient, getAuthLink } from './utils';
 
 const ENV = dotenv.config({ path: __dirname + '/../../.env' }).parsed!;
 
-commander
-  .option('--request-token <callback>', 'Ask for OAuth 1.0a request token for callback <callback>')
-  .option('--access-token <verifier>', 'Ask for OAuth 1.0a access token with verifier <verifier>. Use access tokens from .env')
-.parse(process.argv);
-
 (async () => {
-  if (commander.accessToken) {
-    const client = await getAccessClient(commander.accessToken);
+  const argActive = (name: string) => process.argv.find(arg => arg.startsWith('--' + name));
+  const argValue = (name: string) => argActive(name)?.split(' ', 2)[1];
+
+  if (argActive('access-token')) {
+    const client = await getAccessClient(argValue('access-token')!);
     console.log(client.getActiveTokens());
   }
-  else if (commander.requestToken) {
-    console.log(await getAuthLink(commander.requestToken));
+  else if (argActive('request-token')) {
+    console.log(await getAuthLink(argValue('request-token')!));
   }
 })().catch(e => {
   console.error('Unexcepted error:', e);
