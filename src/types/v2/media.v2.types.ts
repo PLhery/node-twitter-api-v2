2db660cdcc02219d78ece5c1a875227852a58c61
leeran7 <leeran7@gmail.com>
2025-02-24 11:51:10 -0500

    fix(upload-media): use form-data headers, better typing

--- src/types/v2/media.v2.types.ts
+++ src/types/v2/media.v2.types.ts
@@ -1,15 +1,17 @@
+export type MediaV2MediaCategory = 'tweet_image' | 'tweet_video' | 'tweet_gif' | 'dm_image' | 'dm_video' | 'dm_gif' | 'subtitles';
+
 export interface MediaV2UploadInitParams {
   command: 'INIT';
   media_type: string;
   total_bytes: number;
-  media_category?: string;
+  media_category?: MediaV2MediaCategory;
 }
 
 export interface MediaV2UploadAppendParams {
   command: 'APPEND';
   media_id: string;
   segment_index: number;
-  media: Buffer | string;
+  media: Buffer;
 }
 
 export interface MediaV2UploadFinalizeParams {
