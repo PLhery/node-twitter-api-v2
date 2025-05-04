039ecf6949bdebba76c19d954cd63d71478f6fe0
Antonio Villegas <lotendil@gmail.com>
2025-05-04 21:06:18 +0200

    Update uploadMedia to use new dedicated endpoints for chunked media upload in V2 (#584)

--- src/types/v2/media.v2.types.ts
+++ src/types/v2/media.v2.types.ts
@@ -1,22 +1,14 @@
 export type MediaV2MediaCategory = 'tweet_image' | 'tweet_video' | 'tweet_gif' | 'dm_image' | 'dm_video' | 'dm_gif' | 'subtitles';
 
 export interface MediaV2UploadInitParams {
-  command: 'INIT';
   media_type: string;
   total_bytes: number;
   media_category?: MediaV2MediaCategory;
 }
 
 export interface MediaV2UploadAppendParams {
-  command: 'APPEND';
-  media_id: string;
   segment_index: number;
   media: Buffer;
-}
-
-export interface MediaV2UploadFinalizeParams {
-  command: 'FINALIZE';
-  media_id: string;
 }
 
 export interface MediaV2ProcessingInfo {
