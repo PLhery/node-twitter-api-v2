345cc0fa7f34c60390708717fb7b1cc0cda85428
Paul-Louis HERY <paullouis.hery@gmail.com>
2025-09-04 21:17:14 +0200

    test: add community v2 endpoint coverage

--- src/types/v2/community.v2.types.ts
+++ src/types/v2/community.v2.types.ts
@@ -1,7 +1,30 @@
+export interface CommunityRuleV2 {
+  /** Identifier of the rule. */
+  id: string;
+  /** Text of the rule. */
+  text: string;
+  /** Creation date of the rule. */
+  created_at?: string;
+}
+
 export interface CommunityV2 {
   id: string;
   name: string;
+  /** Description of the community. */
+  description?: string;
+  /** Identifier of the community creator. */
+  creator_id?: string;
   created_at: string;
+  /** Indicates if the community is private. */
+  private?: boolean;
+  /** Number of members in the community. */
+  member_count?: number;
+  /** Number of moderators in the community. */
+  moderator_count?: number;
+  /** Number of subscribers of the community. */
+  subscriber_count?: number;
+  /** Rules that apply to the community. */
+  rules?: CommunityRuleV2[];
 }
 
 export interface CommunityErrorV2 {
